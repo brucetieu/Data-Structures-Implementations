52522002621f3c9f9c85330795d4e73dcac51986
brucetieu <brucetieu5@gmail.com>
2020-12-16 14:16:05 -0700

    Add MaxPriorityQueue and MinPriorityQueue

---
+++ Queue/MaxPriorityQueue.py
@@ -0,0 +1,85 @@
+'''
+Define a Node class which will go in the queue.
+'''
+class Node:
+    def __init__(self, data, priority):
+        self.data = data
+        self.priority = priority
+
+'''
+Minimum Priority Queue class: an element with high priority is served before an element with low priority
+'''
+class MaxPriorityQueue:
+    def __init__(self):
+        self.queue = []
+
+    '''
+    Insert nodes to the front of the queue.
+    '''
+    def enqueue(self, node):
+        self.queue.insert(0, node)
+
+    '''
+    Remove items from the queue with the lowest priority.
+    '''
+    def dequeue(self):
+        hashmap = {}
+
+        if self.is_empty():
+            print('Queue is already empty, nothing to dequeue')
+        else:
+            max = float('-inf')
+            idx = 0
+
+            # Traverse through the queue and locate the node with the lowest priority.
+            for i in range(len(self.queue)):
+                if self.queue[i].priority > max:
+                    max = self.queue[i].priority
+                    idx = i
+            
+            # Remove this from the queue.
+            max_priority = self.queue.pop(idx)
+            hashmap[max_priority.data] = max_priority.priority
+            print("Dequeued:", hashmap)
+
+    ''' 
+    Get the first element in the queue.
+    '''
+    def peek(self):
+        hashmap = {}
+        if self.is_empty():
+            print('Queue is already empty, nothing to peek')
+        top = self.queue[-1]
+        
+        hashmap[top.data] = top.priority
+        return hashmap
+
+    '''
+    Check if the queue is empty.
+    '''
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    ''' 
+    Get the number of elements in the queue.
+    '''
+    def get_size(self):
+        return len(self.queue)
+
+
+my_min_priorityQ = MaxPriorityQueue()
+
+my_min_priorityQ.enqueue(Node("C++", 3))
+my_min_priorityQ.enqueue(Node("Python",1))
+my_min_priorityQ.enqueue(Node("Java", 15))
+my_min_priorityQ.enqueue(Node("C#", 22))
+my_min_priorityQ.enqueue(Node("JavaScript", 5))
+my_min_priorityQ.enqueue(Node("C", 32))
+
+my_min_priorityQ.dequeue()
+my_min_priorityQ.dequeue()
+my_min_priorityQ.dequeue()
+
+
+print("First element in priority queue:", my_min_priorityQ.peek()) 
+
