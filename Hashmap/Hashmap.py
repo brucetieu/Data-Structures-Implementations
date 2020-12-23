757d729269cf99bd120585e4a858ddab17cca6a4
brucetieu <brucetieu5@gmail.com>
2020-12-23 10:58:23 -0700

    Add remove(key) method

--- Hashmap/Hashmap.py
+++ Hashmap/Hashmap.py
@@ -2,19 +2,27 @@
 Create a Hashmap from scratch using arrays.
 '''
 class Hashmap:
-    def __init__(self):
-        self.MAX_SIZE = 10
+    def __init__(self, max_size = 10):
+        self.MAX_SIZE = max_size
         self.hashmap = [None] * self.MAX_SIZE
 
+    '''
+    We are hasing the value of the key, a string, to an index.
+    '''
     def get_hash(self, key):
+
+        # Formula: sum of each letters ASCII modded by length of the string.
         hash = 0
         for s in key:
             hash += ord(s)
         return hash % len(key)
 
+    '''
+    Add key value pair to hashmap.
+    '''
     def add(self, key, value):
-        hashed_key = 0
         
+        # Hash the string to an index.
         hashed_key = self.get_hash(key)
 
         # If key doesn't already exist in hash map, add the key value pair as a list. e.g [[key,value]]
@@ -35,14 +43,30 @@ class Hashmap:
             self.hashmap.append([key, value])
             return True
 
+    '''
+    Get the value of the key.
+    '''
     def get(self, key):
         for pairs in self.hashmap:
             if pairs is not None:
                 if pairs[0] == key:
                     return pairs[1]
-        return -1
+        return -1 # Not found
+
+    '''
+    Remove a pair with the key value specified.
+    '''
+    def remove(self, key):
+        for idx, pairs in enumerate(self.hashmap):
+            if pairs is not None:
+                if pairs[0] == key:
+                    self.hashmap.pop(idx)
+                    return True
+        return False
 
 
+
+# Test hashmap.
 myHashMap = Hashmap()
 
 myHashMap.add('1', 1)
@@ -52,3 +76,10 @@ myHashMap.add("Python", '3.8.3')
 print(myHashMap.get('1'))
 print(myHashMap.get('name'))
 print(myHashMap.get("Python"))
+
+myHashMap.remove('1')
+myHashMap.remove('name')
+myHashMap.remove('Python')
+
+myHashMap.add("Python", 'Code')
+print(myHashMap.get('Python'))
