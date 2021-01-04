2d25c58602d8c84f7810bf352a7245472a2a71b5
brucetieu <brucetieu5@gmail.com>
2021-01-04 07:35:42 -0700

    Remove locate_min_child()

--- BinaryHeap/MinBinaryHeap.py
+++ BinaryHeap/MinBinaryHeap.py
@@ -30,27 +30,6 @@ class MinBinaryHeap:
             self.bubble_up(last_idx)
 
 
-    def locate_min_child(self, index):
-        left_child, right_child = None, None
-
-        if (2 * index) + 1 < len(self._min_binary_heap):
-            left_child = self._min_binary_heap[(2 * index) + 1]
-        if (2 * index) + 2 < len(self._min_binary_heap):
-            right_child = self._min_binary_heap[(2 * index) + 2]
-
-        if left_child is None:
-            return right_child
-        elif right_child is None:
-            return left_child
-        else:
-            return min(left_child, right_child)
-
-        
-
-
-
-
-
 
     def find_min(self):
         if len(self._min_binary_heap) == 0:
