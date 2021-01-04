f791647ee17d9122cdcd076a44352270a32d4cfb
brucetieu <brucetieu5@gmail.com>
2021-01-04 07:34:28 -0700

    Add trickle_down() when removing min from heap

--- BinaryHeap/MinBinaryHeap.py
+++ BinaryHeap/MinBinaryHeap.py
@@ -10,9 +10,16 @@ class MinBinaryHeap:
             index = (index - 1) // 2
 
 
-    
-    def trickle_down(self):
-        pass
+    def trickle_down(self, index):
+        
+        while (2 * index) + 2 < len(self._min_binary_heap) or (2 * index) + 1 < len(self._min_binary_heap):
+            if self._min_binary_heap[index] > self._min_binary_heap[(2 * index) + 1]:
+                self._min_binary_heap[index], self._min_binary_heap[(2 * index) + 1] = self._min_binary_heap[(2 * index) + 1], self._min_binary_heap[index]
+                index = (2 * index) + 1
+            elif self._min_binary_heap[index] > self._min_binary_heap[(2 * index) + 2]:
+                self._min_binary_heap[index], self._min_binary_heap[(2 * index) + 1] = self._min_binary_heap[(2 * index) + 1], self._min_binary_heap[index]
+                index = (2 * index) + 2
+
 
     def insert(self, val):
         if len(self._min_binary_heap) == 0:
@@ -23,12 +30,41 @@ class MinBinaryHeap:
             self.bubble_up(last_idx)
 
 
+    def locate_min_child(self, index):
+        left_child, right_child = None, None
 
-    def min_child(self):
-        pass
+        if (2 * index) + 1 < len(self._min_binary_heap):
+            left_child = self._min_binary_heap[(2 * index) + 1]
+        if (2 * index) + 2 < len(self._min_binary_heap):
+            right_child = self._min_binary_heap[(2 * index) + 2]
+
+        if left_child is None:
+            return right_child
+        elif right_child is None:
+            return left_child
+        else:
+            return min(left_child, right_child)
+
+        
+
+
+
+
+
+
+    def find_min(self):
+        if len(self._min_binary_heap) == 0:
+            print("Binary heap is empty, no minimum element exists.")
+        return self._min_binary_heap[0]
 
     def remove_min(self):
-        pass
+        if len(self._min_binary_heap) == 0:
+            print("Binary heap is empty, nothing to remove.")
+
+        else:
+            self._min_binary_heap[0] = self._min_binary_heap[len(self._min_binary_heap) - 1]
+            self._min_binary_heap.pop()
+            self.trickle_down(0)
 
     def build_heap(self):
         pass
@@ -39,12 +75,8 @@ class MinBinaryHeap:
 
 my_min_heap = MinBinaryHeap()
 
-my_min_heap.insert(10)
-my_min_heap.insert(5)
-my_min_heap.insert(11)
-my_min_heap.insert(7)
-my_min_heap.insert(3)
-my_min_heap.print_heap()
+
+
 
 
     
