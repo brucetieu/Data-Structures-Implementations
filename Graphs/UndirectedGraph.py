acec6f6f36522d0cb3d5b2a78c865cb336f2bf14
brucetieu <brucetieu5@gmail.com>
2021-01-30 11:38:59 -0700

    Add addEdge(), print_graph()

--- Graphs/UndirectedGraph.py
+++ Graphs/UndirectedGraph.py
@@ -20,4 +20,47 @@ class UndirectedGraph:
             self.__init__(int(f.readline()))
             self.E = int(f.readline())
 
+            for i in range(self.E):
+                v, w = map(int, f.readline().split())
+                self.addEdge(v, w)
 
+
+    def addEdge(self, v, w):
+        node = self.AdjNode(w)
+
+        if self.undirectedGraph[v] is None:
+            self.undirectedGraph[v] = node
+        else:
+            temp = self.undirectedGraph[v]
+
+            while temp.next != None:
+                temp = temp.next
+            temp.next = node
+
+        node = self.AdjNode(v)
+
+        if self.undirectedGraph[w] is None:
+            self.undirectedGraph[w] = node
+        else:
+            temp = self.undirectedGraph[w]
+
+            while temp.next != None:
+                temp = temp.next
+            temp.next = node
+        
+
+        self.E += 1
+
+    def print_graph(self):
+        for i in range(self.V):
+            print("Vertex " + str(i) + ":", end="")
+            temp = self.undirectedGraph[i]
+            while temp:
+                print(" -> {}".format(temp.V), end="")
+                temp = temp.next
+            print(" \n")
+
+
+
+myGraph = UndirectedGraph(None, 'tinyG.txt')
+myGraph.print_graph()
