8f5c86af05350ffe544f1ef19d9a60cc0169f845
brucetieu <brucetieu5@gmail.com>
2021-01-31 10:57:02 -0700

    Add numberOfSelfLoops()

--- Graphs/GraphProcessingClient.py
+++ Graphs/GraphProcessingClient.py
@@ -5,18 +5,12 @@ class GraphProcessingClient:
     1. degree(Graph, v): Compute the degree of v (number of edges connected to v)
     2. maxDegree(Graph): Compute the max degree of Graph
     3. averageDegree(Graph): Compute average degree of a Graph (The average number of edges per node in the graph)
-    4. numberOfSelfLoops(Graph): Count the number of self loops (An edge of a graph which starts and ends at the same vertex) of a Graph
+    4. numberOfSelfLoops(Graph): Count the number of self loops (An edge of a graph which starts and ends at the same vertex of a Graph)
     5. print_graph: Print a string representation of the adjacency list
     '''
 
     def degree(self, G, v):
-        degree = 0
-
-        for i in range(G.adj(v)):
-            degree += 1
-
-        return degree
-
+        return G.adj(v)
 
     def maxDegree(self, G):
         max = 0
@@ -32,22 +26,25 @@ class GraphProcessingClient:
     def averageDegree(self, G):
 
         # Total Edges/Total Nodes=Average Degree
-        # 2 b/c each edge adjacent to 2 vertices (counted twice)
-        return 2 * G.num_edges() // G.num_vertices()
+        return G.num_edges() // G.num_vertices()
  
-
     def numberOfSelfLoops(self, G):
-        pass
+        self_loops = 0
 
-    def print_graph(self):
-        pass
+        for v in range(G.num_vertices()):
+            for w in range(G.adj(v)):
+                if v == w:
+                    self_loops += 1
+
+        return self_loops // 2
 
 
 graph_client = GraphProcessingClient()
 myGraph = UndirectedGraph(None, 'tinyG.txt')
 
-print(graph_client.degree(myGraph, 1))
-print(graph_client.maxDegree(myGraph))
-print(graph_client.averageDegree(myGraph))
+print("Degree:", graph_client.degree(myGraph, 1))
+print("Max degree:", graph_client.maxDegree(myGraph))
+print("Average degree:", graph_client.averageDegree(myGraph))
+print("Self loops:", graph_client.numberOfSelfLoops(myGraph))
 
 
