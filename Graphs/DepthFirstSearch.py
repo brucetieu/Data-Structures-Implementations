be7ae275773dc634c67b72e15774ba15f77a8bf3
brucetieu <brucetieu5@gmail.com>
2021-01-31 11:38:59 -0700

    Add DFS API skeleton

---
+++ Graphs/DepthFirstSearch.py
@@ -0,0 +1,25 @@
+class DepthFirstSearch:
+    '''Perform a dfs to search in a connected graph. 
+    Algorithm:
+        1. When visiting a vertex, mark it as visited
+        2. Visit (recursively) all the vertices that are adjacent to it and that have not yet been marked.
+    '''
+
+    # Find the vertices in the graph that are connected to the source. 
+    def __init__(self, G, source):
+        self.marked = [False] * G.num_vertices()  # A list of booleans which tells us if a vertex is connected to an edge
+        self.count = 0    # Tells us how many vertices are connected to the source
+
+        self.dfs(G, source)
+
+
+    def marked(self, v):
+        pass
+
+    def count(self):
+        pass
+
+    def dfs(self, G, v):
+        pass
+
+
