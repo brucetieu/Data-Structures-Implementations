60616fe99875ea190cc8e98bbab540f54dc960bb
brucetieu <brucetieu5@gmail.com>
2021-02-02 14:11:35 -0700

    Paths API

---
+++ Graphs/Paths.py
@@ -0,0 +1,16 @@
+# Given a graph and a source vertex s, support queries of the
+# form Is there a path from s to a given target vertex v? If so, find such a path.
+
+class Paths:
+
+    # Find paths in G from source s.
+    def __init__(self, G, s):
+        pass
+
+    # Is there a path from s to v?
+    def hasPathTo(self, v):
+        pass
+    
+    # Path from s to v; null if no such path.
+    def pathTo(self, v):
+        pass
