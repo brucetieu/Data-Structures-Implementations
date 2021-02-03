62784873f941b019de9cbd1197a5cfafd41eb327
brucetieu <brucetieu5@gmail.com>
2021-02-03 11:22:41 -0700

    Fix bfs

--- Graphs/BreadthFirstPaths.py
+++ Graphs/BreadthFirstPaths.py
@@ -9,42 +9,65 @@ class BreadthFirstPaths:
         self.bfs(G, self.s)
 
 
-    def bfs(self, G, v):
+    def bfs(self, G, s):
 
         # Use a queue to perform bfs.
         q = queue.Queue(G.num_vertices())
 
         # Mark source as visited.
-        self.marked[v] = True
+        self.marked[s] = True
 
         # Source goes first in the queue.
-        q.put(v)
+        q.put(s)
 
         while not q.empty():
             vertex = q.get()
 
             # Get first adjacent vertex to v.
-            node = vertex.adj(v)
+            node = G.adj(vertex)
 
-            # Enqueue remaining adjacent vertices, mark them as visited if they haven't yet been visited, and remember where they came from.
             while node is not None:
 
                 if not self.marked[node.V]:
                     q.put(node.V)
                     self.marked[node.V] = True
-                    self.edgeTo[node.V] = v
+                    self.edgeTo[node.V] = vertex  # save last edge on a shortest path
 
                 node = node.next
 
         
-
-
-
     def hasPathTo(self, v):
-        pass
+        return self.marked[v]
 
     def pathTo(self, v):
-        pass
+        stack = []
+
+        while v != self.s:
+            stack.append(v)
+            v = self.edgeTo[v]
+
+        stack.append(self.s)
+
+        return stack
+
+
+myGraph = UndirectedGraph(None, 'tinyCG.txt')
+s = 0
+search = BreadthFirstPaths(myGraph, s)
+
+
+# Find the paths from s to every other vertex, if any path exists.
+for i in range(myGraph.num_vertices()):
+    print("{} to {}: ".format(s, i), end="")
+
+    # Use [::-1] to reverse the stack.
+    for w in search.pathTo(i)[::-1]:
+        if search.hasPathTo(w):
+            print("{}-".format(w), end="")
+    print()
+
+
+
 
 
     
