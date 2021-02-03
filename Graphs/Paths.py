53155c6463e42ebb030161f10b2de3188e7c5512
brucetieu <brucetieu5@gmail.com>
2021-02-03 09:43:39 -0700

    print out all paths from s to every other vertex

--- Graphs/Paths.py
+++ Graphs/Paths.py
@@ -1,3 +1,5 @@
+from UndirectedGraph import UndirectedGraph
+
 # Given a graph and a source vertex s, support queries of the
 # form Is there a path from s to a given target vertex v? If so, find such a path.
 
@@ -6,7 +8,7 @@ class Paths:
     # Find paths in G from source s.
     def __init__(self, G, s):
         self.marked = [False] * G.num_vertices()
-        self.edgeTo = [None] * G.num_vertices()
+        self.edgeTo = [0] * G.num_vertices()
         self.s = s
         self.dfs(G, self.s)
 
@@ -34,7 +36,7 @@ class Paths:
     # Depth first search from v and remember the edge that takes us to each vertex the first time.
     def dfs(self, G, v):
         
-        self.marked[s] = True
+        self.marked[v] = True
 
         node = G.adj(v)
 
@@ -43,8 +45,24 @@ class Paths:
 
                 # To get to this vertex, we had to come from v. Think of s being the root of a tree, and it's branches are paths which all connect to s.
                 self.edgeTo[node.V] = v
-                self.dfs(node.V)
+                self.dfs(G, node.V)
 
             node = node.next
+
+
+myGraph = UndirectedGraph(None, 'tinyCG.txt')
+s = 0
+search = Paths(myGraph, s)
+
+
+# Find the paths from s to every other vertex, if any path exists.
+for i in range(myGraph.num_vertices()):
+    print("{} to {}: ".format(s, i), end="")
+
+    # Use [::-1] to reverse the stack.
+    for w in search.pathTo(i)[::-1]:
+        if search.hasPathTo(w):
+            print("{}-".format(w), end="")
+    print()
 
 
