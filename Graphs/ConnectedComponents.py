e85be280fabbb21ce65de20345c13e2f5f188783
brucetieu <brucetieu5@gmail.com>
2021-02-04 10:17:02 -0700

    Add ConnectedComponents API

---
+++ Graphs/ConnectedComponents.py
@@ -0,0 +1,20 @@
+from UndirectedGraph import UndirectedGraph
+
+class ConnectedComponents:
+
+    # Preprocessing constructor
+    def __init__(self, G):
+        pass
+
+    
+    # Are v and w connected?
+    def connected(self, v, w):
+        pass
+
+    # Number of connected components
+    def count(self):
+        pass
+
+    # Component identifier for v (between 0 and count() - 1)
+    def id(self, v):
+        pass
