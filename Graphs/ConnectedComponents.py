553807dc7a264b3a3db1aa030d991426a680e954
brucetieu <brucetieu5@gmail.com>
2021-02-04 10:21:19 -0700

    implement constructor

--- Graphs/ConnectedComponents.py
+++ Graphs/ConnectedComponents.py
@@ -4,7 +4,15 @@ class ConnectedComponents:
 
     # Preprocessing constructor
     def __init__(self, G):
-        pass
+        self.marked = [False] * G.num_vertices()
+        self.id = [None] * G.num_vertices()
+        self.count = 0
+
+        for v in range(G.num_vertices()):
+            if not self.marked[v]:
+                self.dfs(G, v)
+                self.count += 1
+    
 
     
     # Are v and w connected?
@@ -17,4 +25,7 @@ class ConnectedComponents:
 
     # Component identifier for v (between 0 and count() - 1)
     def id(self, v):
+        pass
+
+    def dfs(self, G, v):
         pass
