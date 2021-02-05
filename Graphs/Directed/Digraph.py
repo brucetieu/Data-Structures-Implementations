cf7f008a9885340cdb583da09eacfaa5df7c1530
brucetieu <brucetieu5@gmail.com>
2021-02-05 11:48:55 -0700

    Implement AdjNode class and Digraph constructor

--- Graphs/Directed/Digraph.py
+++ Graphs/Directed/Digraph.py
@@ -1,11 +1,26 @@
 class Digraph:
     
     class AdjNode:
-        pass
+        def __init__(self, v):
+            self.v = v  # Value of vertex
+            self.next = None  # Points to the next adjacent vertex
 
     # Create a V-vertex digraph with no edges
     def __init__(self, V=None, file=None):
-        pass
+        if V:
+            self.V = V
+            self.E = 0
+            self.digraph = [None] * self.v
+
+        elif file:
+            f = open(file, 'r')
+            self.__init__(int(f.readline()))
+            E = int(f.readline())
+            for i in range(E):
+                v, w = map(int, f.readline())
+                self.addEdge(v, w)
+                
+
 
     # Number of vertices in the digraph
     def num_vertices(self):
