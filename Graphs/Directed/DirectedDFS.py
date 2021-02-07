b75b47773a5d5029a3547590c4734bb598badc5d
brucetieu <brucetieu5@gmail.com>
2021-02-07 11:35:22 -0700

    Add two constructor, one for single source, another for multiple sources

--- Graphs/Directed/DirectedDFS.py
+++ Graphs/Directed/DirectedDFS.py
@@ -3,10 +3,19 @@ from Digraph import Digraph
 class DirectedDFS:
 
     def __init__(self, G, source=None, sources=None):
+        
+        # Single source reachability
         if source:
-            pass
+            self.marked = [False] * G.num_vertices()
+            self.dfs(G, source)
+
+        # Multiple source reachability
         elif sources:
-            pass
+            self.marked = [False] * G.num_vertices()
+            
+            for s in sources:
+                if not self.marked[s]:
+                    self.dfs(G, s)
 
     def visited(self):
         pass
