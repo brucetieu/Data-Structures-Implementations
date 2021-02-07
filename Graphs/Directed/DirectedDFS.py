d961479fd191fb883bd01a3fbb6f130955b14283
brucetieu <brucetieu5@gmail.com>
2021-02-07 11:32:04 -0700

    Add DirectedDFS

---
+++ Graphs/Directed/DirectedDFS.py
@@ -0,0 +1,15 @@
+from Digraph import Digraph
+
+class DirectedDFS:
+
+    def __init__(self, G, source=None, sources=None):
+        if source:
+            pass
+        elif sources:
+            pass
+
+    def visited(self):
+        pass
+
+    def dfs(self, G, s):
+        pass
