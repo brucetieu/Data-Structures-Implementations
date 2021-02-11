5b3eb53589ba00da4c85d738011abd93f9b41973
brucetieu <brucetieu5@gmail.com>
2021-02-11 11:07:39 -0700

    add DirectedCycle API

---
+++ Graphs/Directed/DirectedCycle.py
@@ -0,0 +1,15 @@
+from Digraph import Digraph
+
+class DirectedCycle:
+
+    # Cycle finding constructor
+    def __init__(self, G):
+        pass
+
+    # Does G have a directed cycle?
+    def hasCycle(self):
+        pass
+
+    # An iterable containing all vertices on the cycle
+    def cycle(self):
+        pass
