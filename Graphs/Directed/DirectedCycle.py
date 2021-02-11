39f232fd106bba8ba6433ba96de15f730dac7be9
brucetieu <brucetieu5@gmail.com>
2021-02-11 11:15:00 -0700

    Implement constructor, hasCycle(), cycle()

--- Graphs/Directed/DirectedCycle.py
+++ Graphs/Directed/DirectedCycle.py
@@ -4,12 +4,17 @@ class DirectedCycle:
 
     # Cycle finding constructor
     def __init__(self, G):
-        pass
+        self.marked = [None] * G.num_vertices()
+        self.onStack = [False] * G.num_vertices()  # Vertices on the recursive call stack
+        self.cycles = []  # Store the cycles
 
+    def dfs(self, G, v):
+        pass
+    
     # Does G have a directed cycle?
     def hasCycle(self):
-        pass
+        return len(self.cycles) > 0
 
     # An iterable containing all vertices on the cycle
     def cycle(self):
-        pass
+        return self.cycles
