dae44d73925961c56c7fe4d359a41ad0855a9049
brucetieu <brucetieu5@gmail.com>
2021-02-11 11:30:56 -0700

    Finish DirectedCycle

--- Graphs/Directed/DirectedCycle.py
+++ Graphs/Directed/DirectedCycle.py
@@ -6,15 +6,45 @@ class DirectedCycle:
     def __init__(self, G):
         self.marked = [None] * G.num_vertices()
         self.onStack = [False] * G.num_vertices()  # Vertices on the recursive call stack
-        self.cycles = []  # Store the cycles
+        self.isCycle = False  # Store the cycles
+
+        for v in range(G.num_vertices()):
+            self.dfs(G, v)
 
     def dfs(self, G, v):
-        pass
-    
+        
+        self.marked[v] = True
+
+        # Current vertex is on the stack
+        self.onStack[v] = True
+
+        node = G.adj(v)
+
+        while node:
+
+            # There is a path from w->v => a cycle exists
+            if self.onStack[node.v]: 
+                self.isCycle = True
+            if not self.marked[node.v]:
+                self.dfs(G, node.v)
+            
+            node = node.next
+        
+        # reset
+        self.onStack[v] = False
+
     # Does G have a directed cycle?
     def hasCycle(self):
-        return len(self.cycles) > 0
+        return self.isCycle
 
-    # An iterable containing all vertices on the cycle
-    def cycle(self):
-        return self.cycles
+
+
+# Test
+myGraph = Digraph(None, 'tinyDG.txt')
+myGraph2 = Digraph(None, 'tinyDAG.txt')
+
+directed_cycle = DirectedCycle(myGraph)
+directed_cycle2 = DirectedCycle(myGraph2)
+
+print(directed_cycle.hasCycle())
+print(directed_cycle2.hasCycle())
